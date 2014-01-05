a0164ade301183c325ebf3ac8551972111328425
hoge <hoge@hoge.com>
2014-01-05 18:55:59 +0900

    リファクタリング

--- src/com/akidn8/android/bluetoothsppreciver/MainActivity.java
+++ src/com/akidn8/android/bluetoothsppreciver/MainActivity.java
@@ -25,32 +25,23 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_main);
-//	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
+	// ログ出力用TAG
     private static final String LOG_TAG ="BT_Arduino";
     
-    /* SPPで繋げる時のUUIDは決まっている模様 */
-    //private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // SPPで繋げる時のUUIDは決まっている模様
     private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     
+    // Bluetooth通信用
     private BluetoothDevice btDevice;
     private BluetoothSocket btSocket;
  
-    private Thread thread;
-    private Timer uiTimer; //UI描画用タイマー
+    // Bluetoothデータ受信スレッド 
+    private Thread threadBTCom; 
+    // スレッド終了フラグ
+    boolean halt = false;
  
-    boolean isThreadStop = false;
+    //TextViewに表示する文字列
+    public String str_txtview = ""; 
     
     @Override
     public void onResume(){
@@ -65,13 +56,30 @@ public class MainActivity extends Activity {
         initBT();
         
         Handler handler = new Handler();
-        thread = createMyThread(handler);
-        thread.start();
-//        uiTimer = new Timer();
-        uiTimer = createUITimer(handler);
+        threadBTCom = createBTComThread(handler);
+        threadBTCom.start();
+        startUIUpdater(handler);
     }
-
- 
+    
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.activity_main, menu);
+//		return true;
+//	}
+//
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        halt = true;
+        try {
+            btSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     private void initBT(){
 
         // Android端末ローカルのBTアダプタを取得
@@ -83,21 +91,21 @@ public class MainActivity extends Activity {
  
         while(it.hasNext()){
           btDevice = it.next();
-          Log.e(LOG_TAG, "btAddr = " + btDevice.getAddress());
-          Log.e(LOG_TAG, "btAddr = " + btDevice.getName());
+          Log.d(LOG_TAG, "btAddr = " + btDevice.getAddress());
+          Log.d(LOG_TAG, "btAddr = " + btDevice.getName());
 ///          if (btDevice.getName().equals("EasyBT")){
           if (btDevice.getName().equals("BTCOM-SPPB")){
-        	  Log.e(LOG_TAG, "BTCOM-SPPB Try connection.");
+        	  Log.d(LOG_TAG, "BTCOM-SPPB Try connection.");
         	  if (tryBTConnection()){
-            	  Log.e(LOG_TAG, "BTCOM-SPPB Connected!!!");
+            	  Log.d(LOG_TAG, "BTCOM-SPPB Connected!!!");
         		  break;
         	  }
           }
           if (btDevice.getName().equals("SBDBT-001bdc0f7281")){
-//        	  Log.e(LOG_TAG, "SBDBT-001bdc0f7281 FOUND!!");
-        	  Log.e(LOG_TAG, "SBDBT-001bdc0f7281 Try connection.");
+//        	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 FOUND!!");
+        	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 Try connection.");
         	  if (tryBTConnection()){
-            	  Log.e(LOG_TAG, "SBDBT-001bdc0f7281 Connected!!!");
+            	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 Connected!!!");
         		  break;
         	  }
           }
@@ -107,121 +115,83 @@ public class MainActivity extends Activity {
     
     private boolean tryBTConnection(){
         try {
-        	
-        	if (false){
-                // RFCOMM用のSocketを生成
-        		// こっちだとモジュールによって失敗する
-                btSocket = btDevice.createRfcommSocketToServiceRecord(uuid);
-                btSocket.connect();
-        	}else{
-                // StuckOverFlowにあった解決法
-        		// これだと成功。意味はわからん
-//              BluetoothDevice hxm = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(device.getAddress());
-              BluetoothDevice hxm = btDevice;
-              Method m;
-              m = hxm.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
-              btSocket = (BluetoothSocket)m.invoke(hxm, Integer.valueOf(1)); 
-              btSocket.connect();
-        	} 
+			BluetoothDevice hxm = btDevice;
+			Method m;
+			m = hxm.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
+			btSocket = (BluetoothSocket)m.invoke(hxm, Integer.valueOf(1)); 
+			btSocket.connect();
             Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
             return true;
         } catch (IOException e) {
             e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
         } catch (SecurityException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
 		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
 		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
             Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
 		}
         return false;
     }
     
-    
-    public String str = "";
-    public String str_txtview = "";
-    
-    private Thread createMyThread(final Handler handler){
-    	
-//		final TextView tv = (TextView)findViewById(R.id.txtMain);
-//		final Runnable updateUI = new Runnable(){
-//			@Override
-//			public void run() {
-//				tv.setText(str_txtview);
-//			}
-//		};
+    private Thread createBTComThread(final Handler handler){
     	
     	return new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                	while(true){
-    					Log.e(LOG_TAG, "while1...top");
-    					InputStream inStream = btSocket.getInputStream();
-//    					int size=inStream.read();
+					InputStream inStream = btSocket.getInputStream();
+                	while(!halt){
     					int size=0;
     					int maxsize = 1024;
     					final byte[] buffer = new byte[maxsize];
-    					Log.e(LOG_TAG, "while1...");
-    					while(true){
-//    						int rsize = inStream.read(buffer, size, maxsize-size);
+    					while(!halt){
+    						// 改行コードが出てくるまで読み込み続ける
     						if (size+1 > maxsize){
     							//これ以上は読めない
     							//エラー
     							break;
     						}
+    						// 読み込み
     						int rsize = inStream.read(buffer, size, 1);
+
+							//改行が出てきたか判断
     						final int CODE_LF = 10; //改行コード
     						size  = size + rsize;
     						if (buffer[size-1]==CODE_LF){
-    							//改行が出てきたら、出力
-    							DataInputStream in = new DataInputStream(new ByteArrayInputStream(buffer));
-    							str = new String(buffer);
-    							String ss = str.substring(0, size);
-    							str_txtview = ss + str_txtview;
-//    							str_txtview = "1234567890\n" + str_txtview;
+    							// 改行が出てきたら、表示文字列を編集してwhile抜ける
+    							String str = new String(buffer);
+    							str_txtview = str.substring(0, size) + str_txtview;
     							final int str_txtview_maxlen =  1024*20;
     							if (str_txtview.length() > str_txtview_maxlen){
-    								//長いので程々にカット
+    								//TextViewに表示する文字列が長すぎる時は後ろ部分を切り捨てる
     								str_txtview = str_txtview.substring(0, str_txtview_maxlen); 
     							}
-    							// UI描画命令
-//    							handler.removeCallbacks(updateUI);
-//    							handler.post(updateUI);
-//    							Thread.sleep(5);
     							break;
     						}
     					}
-                		
                 	}
+                	Log.d(LOG_TAG, "Thread ends.");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-//                catch (InterruptedException e){
-//					e.printStackTrace();
-//				}
             }
         });
     }
     
     // UI描画用タイマー生成
-    private Timer createUITimer(final Handler handler){
+    private void startUIUpdater(final Handler handler){
 		final TextView tv = (TextView)findViewById(R.id.txtMain);
 		final Runnable updateUI = new Runnable(){
 			@Override
@@ -235,23 +205,8 @@ public class MainActivity extends Activity {
 			public void run() {
 				handler.removeCallbacks(updateUI);
 				handler.post(updateUI);
-//				tv.setText(str_txtview);
 			}
     	}, 0, 50);
-    	return timer;
     }
-    
-    @Override
-    protected void onPause() {
-        super.onPause();
-        isThreadStop = true;
-        thread.stop();
-        try {
-            btSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
 	
 }
