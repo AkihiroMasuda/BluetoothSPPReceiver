c28a954d9774df1e9b7e37e42f040298d6a74eff
hoge <hoge@hoge.com>
2014-01-05 19:40:13 +0900

    Bluetoothデバイスをリストから選択して接続出来るように変更

--- src/com/akidn8/android/bluetoothsppreciver/MainActivity.java
+++ src/com/akidn8/android/bluetoothsppreciver/MainActivity.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -22,7 +25,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	// ログ出力用TAG
-    private static final String LOG_TAG ="BT_Arduino";
+    private static final String LOG_TAG ="BT_SPP_Receiver";
     
     // Bluetooth通信用
     private BluetoothDevice btDevice;
@@ -45,66 +48,77 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- 
-        initBT();
-        
-        Handler handler = new Handler();
-        threadBTCom = createBTComThread(handler);
-        threadBTCom.start();
-        startUIUpdater(handler);
+
+        //Bluetoothデバイス選択用ダイアログを表示
+        showBTDeviceSeclectDialog();
     }
-    
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.activity_main, menu);
-//		return true;
-//	}
-//
     
     @Override
     protected void onPause() {
         super.onPause();
+        // スレッド終了フラグ
         halt = true;
-        try {
-            btSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.finish();
+        // スレッドが終わった頃を見計らってソケット終了とアプリ終了を実行
+        new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+		        try {
+		            btSocket.close();
+		            MainActivity.this.finish();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+			}
+		},1000); 
     }
     
-    private void initBT(){
+    private void showBTDeviceSeclectDialog(){
 
         // Android端末ローカルのBTアダプタを取得
         BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         // ペアリング済みのデバイス一覧を取得
-        Set<BluetoothDevice> btDeviceSet = btAdapter.getBondedDevices();
+        final Set<BluetoothDevice> btDeviceSet = btAdapter.getBondedDevices();
  
+        // デバイス名を取得してダイアログ表示用の文字列を作成
         Iterator<BluetoothDevice> it = btDeviceSet.iterator();
- 
+        final ArrayList<String> item_list = new ArrayList<String>();
         while(it.hasNext()){
           btDevice = it.next();
           Log.d(LOG_TAG, "btAddr = " + btDevice.getAddress());
           Log.d(LOG_TAG, "btAddr = " + btDevice.getName());
-///          if (btDevice.getName().equals("EasyBT")){
-          if (btDevice.getName().equals("BTCOM-SPPB")){
-        	  Log.d(LOG_TAG, "BTCOM-SPPB Try connection.");
-        	  if (tryBTConnection()){
-            	  Log.d(LOG_TAG, "BTCOM-SPPB Connected!!!");
-        		  break;
-        	  }
-          }
-          if (btDevice.getName().equals("SBDBT-001bdc0f7281")){
-//        	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 FOUND!!");
-        	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 Try connection.");
-        	  if (tryBTConnection()){
-            	  Log.d(LOG_TAG, "SBDBT-001bdc0f7281 Connected!!!");
-        		  break;
-        	  }
-          }
+          
+          item_list.add(btDevice.getName());
         }
 
+        //
+        //処理：メッセージダイアログの表示
+        //
+        new AlertDialog.Builder(MainActivity.this)
+        .setTitle("BTデバイス選択")
+        .setItems(item_list.toArray(new String[item_list.size()]), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // 選択したBluetoothDeviceを保持
+                BluetoothDevice devices[] = btDeviceSet.toArray(new BluetoothDevice[btDeviceSet.size()]);
+                btDevice = devices[which];
+                // Bluetooth通信処理を開始
+                startCommunication();
+            }
+        })
+        .show();
+    }
+    
+    private void startCommunication(){
+    	if (tryBTConnection()){
+    		Log.d(LOG_TAG, "Connected.");
+    		
+            Handler handler = new Handler();
+            threadBTCom = createBTComThread(handler);
+            threadBTCom.start();
+            startUIUpdater(handler);
+    	}else{
+    		Log.e(LOG_TAG, "Bluetooth device connection if failed.");
+    	}
+    	
     }
     
     private boolean tryBTConnection(){
@@ -114,7 +128,7 @@ public class MainActivity extends Activity {
 			m = hxm.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
 			btSocket = (BluetoothSocket)m.invoke(hxm, Integer.valueOf(1)); 
 			btSocket.connect();
-            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, btDevice.getName() + "  connected", Toast.LENGTH_SHORT).show();
             return true;
         } catch (IOException e) {
             e.printStackTrace();
@@ -144,36 +158,38 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 try {
-					InputStream inStream = btSocket.getInputStream();
                 	while(!halt){
-    					int size=0;
-    					int maxsize = 1024;
-    					final byte[] buffer = new byte[maxsize];
-    					while(!halt){
-    						// 改行コードが出てくるまで読み込み続ける
-    						if (size+1 > maxsize){
-    							//これ以上は読めない
-    							//エラー
-    							break;
-    						}
-    						// 読み込み
-    						int rsize = inStream.read(buffer, size, 1);
+                		if (btSocket!=null){
+        					InputStream inStream = btSocket.getInputStream();
+        					int size=0;
+        					int maxsize = 1024;
+        					final byte[] buffer = new byte[maxsize];
+        					while(!halt){
+        						// 改行コードが出てくるまで読み込み続ける
+        						if (size+1 > maxsize){
+        							//これ以上は読めない
+        							//エラー
+        							break;
+        						}
+        						// 読み込み
+        						int rsize = inStream.read(buffer, size, 1);
 
-							//改行が出てきたか判断
-    						final int CODE_LF = 10; //改行コード
-    						size  = size + rsize;
-    						if (buffer[size-1]==CODE_LF){
-    							// 改行が出てきたら、表示文字列を編集してwhile抜ける
-    							String str = new String(buffer);
-    							str_txtview = str.substring(0, size) + str_txtview;
-    							final int str_txtview_maxlen =  1024*20;
-    							if (str_txtview.length() > str_txtview_maxlen){
-    								//TextViewに表示する文字列が長すぎる時は後ろ部分を切り捨てる
-    								str_txtview = str_txtview.substring(0, str_txtview_maxlen); 
-    							}
-    							break;
-    						}
-    					}
+    							//改行が出てきたか判断
+        						final int CODE_LF = 10; //改行コード
+        						size  = size + rsize;
+        						if (buffer[size-1]==CODE_LF){
+        							// 改行が出てきたら、表示文字列を編集してwhile抜ける
+        							String str = new String(buffer);
+        							str_txtview = str.substring(0, size) + str_txtview;
+        							final int str_txtview_maxlen =  1024*20;
+        							if (str_txtview.length() > str_txtview_maxlen){
+        								//TextViewに表示する文字列が長すぎる時は後ろ部分を切り捨てる
+        								str_txtview = str_txtview.substring(0, str_txtview_maxlen); 
+        							}
+        							break;
+        						}
+        					}
+                		}
                 	}
                 	Log.d(LOG_TAG, "Thread ends.");
 				} catch (IOException e) {
